2963f803af588e2b5b390d758e0cb94e671cec62
Werberth Vinicius <werberthvinicius@gmail.com>
2017-12-26 17:57:19 -0300

    Adding game_detail view

--- games/views.py
+++ games/views.py
@@ -38,3 +38,34 @@ def game_list(request):
             game_serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
         )
+
+
+@csrf_exempt
+def game_detail(request, pk):
+    try:
+        game = Game.objects.get(pk=pk)
+    except Game.DoesNotExist:
+        return HttpResponse(
+            status=status.HTTP_404_NOT_FOUND
+        )
+
+    if request.method == 'GET':
+        game_serializer = GameSerializer(game)
+        return JSONResponse(game_serializer.data)
+
+    elif request.method == 'PUT':
+        game_data = JSONParser().parse(request)
+        game_serializer = GameSerializer(game, data=game_data)
+        if game_serializer.is_valid():
+            game_serializer.save()
+            return JSONResponse(game_serializer.data)
+        return JSONResponse(
+            game_serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    elif request.method == 'DELETE':
+        game.delete()
+        return HttpResponse(
+            status=status.HTTP_204_NO_CONTENT
+        )
