936fe8ea5e251ffb6b85ff2b6d4eae6ff8ec5ba2
Werberth Vinicius <werberthvinicius@gmail.com>
2017-12-26 17:44:06 -0300

    Adding game_list view

--- games/views.py
+++ games/views.py
@@ -14,3 +14,27 @@ class JSONResponse(HttpResponse):
         content = JSONRenderer().reder(data)
         kwargs['content_type'] = 'application/json'
         super(JSONResponse, self).__init__(content, **kwargs)
+
+
+@csrf_exempt
+def game_list(request):
+    if request.method == 'GET':
+        games = Game.objects.all()
+        games_serializer = GameSerializer(
+            games, many=True
+        )
+        return JSONResponse(games_serializer.data)
+
+    elif request.method == 'POST':
+        game_data = JSONParser().parse(request)
+        game_serializer = GameSerializer(data=game_data)
+        if game_serializer.is_valid():
+            game_serializer.save()
+            return JSONResponse(
+                game_serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        return JSONResponse(
+            game_serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
