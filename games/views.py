126279aa846aa0e7948b995984a54793a4731aba
Werberth Vinicius <werberthvinicius@gmail.com>
2017-12-26 17:33:05 -0300

    Adding JSONResponse

--- games/views.py
+++ games/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from rest_framework import status
+
+from .models import Game
+from .serializers import GameSerializer
+
+
+class JSONResponse(HttpResponse):
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().reder(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
