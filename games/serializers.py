3ee5aa694c08d1a93fb4ed7be48c7a20411578c0
Werberth Vinicius <werberthvinicius@gmail.com>
2017-12-27 18:02:15 -0300

    Adding GameCategorySerializer

--- games/serializers.py
+++ games/serializers.py
@@ -1,14 +1,41 @@
 from rest_framework import serializers
+from games.models import (
+    GameCategory,
+    Game,
+    Player,
+    PlayerScore
+)
 
-from .models import Game
+
+class GameCategorySerializer(serializers.HyperlinkedModelSerializer):
+    games = serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name='game-detail'
+    )
+
+    class Meta:
+        model = GameCategory
+        fields = (
+            'url',
+            'pk',
+            'name',
+            'games'
+        )
 
 
 class GameSerializer(serializers.ModelSerializer):
+    # We want to display the game category's name instead of the id
+    game_category = serializers.SlugRelatedField(
+        queryset=GameCategory.objects.all(),
+        slug_field='name'
+    )
+
     class Meta:
         model = Game
         fields = (
+            'url',
             'name',
-            'id',
             'release_date',
             'game_category',
             'played'
