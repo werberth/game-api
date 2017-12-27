251ad43d937cda4a51fe6a4d8567180db35e96da
Werberth Vinicius <werberthvinicius@gmail.com>
2017-12-27 15:58:43 -0300

    Adding GameCategory model

--- games/models.py
+++ games/models.py
@@ -1,11 +1,25 @@
 from django.db import models
 
 
+class GameCategory(models.Model):
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
 class Game(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=200, blank=True, default='')
+    game_category = models.ForeignKey(
+        GameCategory,
+        related_name='games',
+        on_delete=models.CASCADE
+    )
     release_date = models.DateTimeField()
-    game_category = models.CharField(max_length=200, blank=True, default='')
     played = models.BooleanField(default=False)
 
     class Meta:
