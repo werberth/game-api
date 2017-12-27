6bd2421dce34149599b5a191b271c494da91c78f
Werberth Vinicius <werberthvinicius@gmail.com>
2017-12-27 16:15:53 -0300

    Adding Player model

--- games/models.py
+++ games/models.py
@@ -24,3 +24,29 @@ class Game(models.Model):
 
     class Meta:
         ordering = ('name',)
+
+
+class Player(models.Model):
+    MALE = 'M'
+    FEMALE = 'F'
+    GENDER_CHOICES = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(
+        max_length=50,
+        blank=True,
+        default=''
+    )
+    gender = models.CharField(
+        max_length=2,
+        choices=GENDER_CHOICES,
+        default=MALE
+    )
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
