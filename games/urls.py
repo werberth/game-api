b1d6c271109fd334c9877a3c09c17b2394d7494f
Werberth Vinicius <werberthvinicius@gmail.com>
2017-12-28 02:40:46 -0300

    Adding routers do User API Views

--- games/urls.py
+++ games/urls.py
@@ -47,5 +47,15 @@ urlpatterns = [
         r'^$',
         views.ApiRoot.as_view(),
         name=views.ApiRoot.name
+    ),
+    url(
+        r'^users/$',
+        views.UserList.as_view(),
+        name=views.UserList.name
+    ),
+    url(
+        r'^users/(?P<pk>[0-9]+)/$',
+        views.UserDetail.as_view(),
+        name=views.UserDetail.name
     )
 ]
