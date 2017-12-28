18caa4a5875121f7e4df75903f3663dbdab509a1
Werberth Vinicius <werberthvinicius@gmail.com>
2017-12-27 21:16:35 -0300

    Excluding old API views, and adding CRUD API views from models: Game, GameCategory, Player, PlayerScore.

--- games/views.py
+++ games/views.py
@@ -1,60 +1,62 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import generics
+
+from .models import (
+    Game,
+    GameCategory,
+    Player,
+    PlayerScore
+)
+from .serializers import (
+    GameSerializer,
+    GameCategorySerializer,
+    PlayerSerializer,
+    PlayerScoreSerializer,
+)
 
 
-from .models import Game
-from .serializers import GameSerializer
+class GameCategoryList(generics.ListCreateAPIView):
+    queryset = GameCategory.objects.all()
+    serializer_class = GameCategorySerializer
+    name = 'gamecategory-list'
 
 
-@api_view(['GET', 'POST'])
-def game_list(request):
-    if request.method == 'GET':
-        games = Game.objects.all()
-        games_serializer = GameSerializer(
-            games, many=True
-        )
-        return Response(games_serializer.data)
-
-    elif request.method == 'POST':
-        game_serializer = GameSerializer(data=request.data)
-        if game_serializer.is_valid():
-            game_serializer.save()
-            return Response(
-                game_serializer.data,
-                status=status.HTTP_201_CREATED
-            )
-        return Response(
-            game_serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+class GameCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = GameCategory.objects.all()
+    serializer_class = GameCategorySerializer
+    name = 'gamecategory-detail'
 
 
-@api_view(['GET', 'PUT', 'POST'])
-def game_detail(request, pk):
-    try:
-        game = Game.objects.get(pk=pk)
-    except Game.DoesNotExist:
-        return Response(
-            status=status.HTTP_404_NOT_FOUND
-        )
+class GameList(generics.ListCreateAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    name = 'game-list'
 
-    if request.method == 'GET':
-        game_serializer = GameSerializer(game)
-        return Response(game_serializer.data)
 
-    elif request.method == 'PUT':
-        game_serializer = GameSerializer(game, data=request.data)
-        if game_serializer.is_valid():
-            game_serializer.save()
-            return Response(game_serializer.data)
-        return Response(
-            game_serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+class GameDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    name = 'game-detail'
 
-    elif request.method == 'DELETE':
-        game.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+
+class PlayerList(generics.ListCreateAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+    name = 'player-list'
+
+
+class PlayerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Player.objects.all()
+    serializers_class = PlayerSerializer
+    name = 'player-detail'
+
+
+class PlayerScoreList(generics.ListCreateAPIView):
+    queryset = PlayerScore.objects.all()
+    serializers_class = PlayerScoreSerializer
+    name = 'playerscore-list'
+
+
+class PlayerScoreDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = PlayerScore.objects.all()
+    serializers_class = PlayerScoreSerializer
+    name = 'playerscore-detail'
