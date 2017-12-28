b0402e97ebbf8d049d1c13eb2c935adac6ed47e1
Werberth Vinicius <werberthvinicius@gmail.com>
2017-12-28 00:28:14 -0300

    Addig Pagination settings to settings.py

--- gameapi/settings.py
+++ gameapi/settings.py
@@ -108,3 +108,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# REST_FRAMEWORK SETTINGS
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS':
+    'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5
+}
